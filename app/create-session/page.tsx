bd6c77c4685aa1c56f2b57db021d85aea7b62223
Evan Holster <ehols001@odu.edu>
2023-11-15 16:21:56 -0800

    created basic create/join pages

---
+++ app/create-session/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateSession() {
+        return (
+                <div className="flex min-h-screen flex-col items-center justify-center p-24">
+                        <p>create session page</p>
+                </div>
+        )
+}
---
+++ app/join-session/page.tsx
@@ -0,0 +1,7 @@
+export default function JoinSession() {
+        return (
+                <div className="flex min-h-screen flex-col items-center justify-center p-24">
+                        <p>join session page</p>
+                </div>
+        )
+}
