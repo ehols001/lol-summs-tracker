6d05a783eb5f61a79cf0a94b6929a1bf8d50417e
Evan Holster <ehols001@odu.edu>
2023-11-21 14:12:04 -0800

    test game data

---
+++ db/test-data.ts
@@ -0,0 +1,109 @@
+import { Match } from "./types"
+
+export const testGame1: Match = {
+        gameId: "1234",
+        gameData: {
+                gameStartTime: new Date("2023-11-19T13:26:36.455Z"),
+                players: [
+                        {
+                                champion: "Riven",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Teleport",
+                                cooldown2: 300,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Kayn",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Smite",
+                                cooldown2: 50,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Ahri",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Ignite",
+                                cooldown2: 240,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Lucian",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Heal",
+                                cooldown2: 240,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Thresh",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Exhaust",
+                                cooldown2: 240,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                ]
+        }
+}
+
+export const testGame2: Match = {
+        gameId: "5678",
+        gameData: {
+                gameStartTime: new Date("2023-11-19T13:26:36.455Z"),
+                players: [
+                        {
+                                champion: "Shen",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Teleport",
+                                cooldown2: 300,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Rammus",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Smite",
+                                cooldown2: 50,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Syndra",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Barrier",
+                                cooldown2: 240,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Ezreal",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Cleanse",
+                                cooldown2: 240,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                        {
+                                champion: "Nami",
+                                summ1: "Flash",
+                                cooldown1: 300,
+                                timeWhenUsed1: new Date("2023-11-19T13:26:36.455Z"),
+                                summ2: "Ignite",
+                                cooldown2: 240,
+                                timeWhenUsed2: new Date("2023-11-19T13:26:36.455Z"),
+                        },
+                ]
+        }
+}
