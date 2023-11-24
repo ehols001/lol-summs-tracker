e5830d4479e51883a952cd9881af71c316500d58
Evan Holster <ehols001@odu.edu>
2023-11-24 11:29:50 -0800

    adjusted styling effected by global color changes

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -10,7 +10,7 @@ export const GameCard = ({
         const players: Player[] = game.gameData.players.map((player) => player);
 
         return (
-                <div className='flex flex-col justify-center items-center w-[320px] h-[500px] bg-gradient-to-br from-cyan-950 to-blue-950 border-2 border-stone-500 rounded-xl'>
+                <div className='flex flex-col justify-center items-center w-[320px] h-[500px]'>
                         {players.map((player) => (
                                 <PlayerCard player={player} key={player.champion} />
                         ))}
