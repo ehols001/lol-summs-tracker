2ac6869655d52c431b9695adee90197f29501a00
eholster <ehols001@odu.edu>
2023-11-27 12:14:36 -0500

    updated size of game card

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -10,7 +10,7 @@ export const GameCard = ({
         const players: Player[] = game.gameData.players.map((player) => player);
 
         return (
-                <div className='flex flex-col justify-center items-center w-[320px] h-[500px]'>
+                <div className='flex flex-col justify-center items-center w-[320px]'>
                         {players.map((player) => (
                                 <PlayerCard player={player} key={player.champion} />
                         ))}
