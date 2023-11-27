e4256e84d4fe443e4ebdf4f921136790c2e28392
eholster <ehols001@odu.edu>
2023-11-27 12:15:03 -0500

    changed format for creating/join sessions (temp)

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,34 @@
 'use client'
 
 import Link from 'next/link'
-import { Input } from 'postcss'
 import React from 'react';
 
 export default function Home() {
-  const [summonerNameValue, setSummonerNameValue] = React.useState('');
-  const [tagLineValue, setTagLineValue] = React.useState('');
-  
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <input name='summonerName' 
-        placeholder='Summoner' 
-        className='bg-slate-400 m-2 p-2 rounded-lg placeholder-white' 
-        onChange={(e) => setSummonerNameValue(e.target.value)}/>
-      
-      <input name='tagLine' 
-        placeholder='Tag Line' 
-        className='bg-slate-400 m-2 p-2 rounded-lg placeholder-white'
-        onChange={(e) => setTagLineValue(e.target.value)}/>
+    const [summonerNameValue, setSummonerNameValue] = React.useState('');
+    const [tagLineValue, setTagLineValue] = React.useState('');
 
-      <Link className='bg-slate-400 m-2 p-2 rounded-lg' 
-        href={{ pathname: '/game/1234', query: {summonerName: summonerNameValue, tagLine: tagLineValue}}}>
-        Join Session
-      </Link>
-    </div>
-  )
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+            <input name='summonerName'
+                placeholder='Summoner'
+                className='bg-slate-400 m-2 p-2 rounded-lg placeholder-white'
+                onChange={(e) => setSummonerNameValue(e.target.value)} />
+
+            <input name='tagLine'
+                placeholder='Tag Line'
+                className='bg-slate-400 m-2 p-2 rounded-lg placeholder-white'
+                onChange={(e) => setTagLineValue(e.target.value)} />
+
+            <Link className='bg-slate-400 m-2 p-2 rounded-lg' href={{ pathname: '/create-session', query: { summonerName: summonerNameValue, tagLine: tagLineValue } }}>
+                Create Session
+            </Link>
+
+            <p className='pt-12'>Already have a game code?</p>
+            <Link className='bg-slate-400 m-2 p-2 rounded-lg'
+                href='/join-session'>
+                Join Session
+            </Link>
+
+        </div>
+    )
 }
