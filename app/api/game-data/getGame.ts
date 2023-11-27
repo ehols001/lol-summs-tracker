7eb894e21d525d79e32b2dba45cf173235f21b12
eholster <ehols001@odu.edu>
2023-11-27 12:16:24 -0500

    added a function to get a game by summoner name

--- app/api/game-data/getGame.ts
+++ app/api/game-data/getGame.ts
@@ -23,3 +23,23 @@ export const getGameByGameId = cache(async (gameId: string) => {
                 console.log(`Failed to find game with gameId: ${gameId}`, error);
         }
 });
+
+/**
+ * Find a specific game given the summonerName 
+ * 
+ * @param summonerName the summonerName of a player in the game
+ * 
+ * @returns full game data including gameId, gameData, and the players array
+ */
+export const getGameBySummonerName = cache(async (summonerName: string) => {
+    await Connect();
+
+    try {
+            const game = await Game.findOne({ 'gameData.players.summonerName': summonerName });
+            console.log(`Successfully found a game with the summoner name: ${summonerName}`, game);
+
+            return game;
+    } catch(error) {
+            console.log(`Failed to find a game with the summoner name: ${summonerName}`, error);
+    }
+});
