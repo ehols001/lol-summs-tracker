51c795dcb0954ad8dcf64ebb5442f2446a1c00a9
eholster <ehols001@odu.edu>
2023-11-28 14:47:34 -0500

    moved game data to its parent page

--- app/game/[team_code]/page.tsx
+++ app/game/[team_code]/page.tsx
@@ -1,6 +1,6 @@
 import { getGameByGameId } from '@/app/api/game-data/getGame';
 import { GameCard } from '@/components/GameCard';
-import { Match } from '@/db/schema';
+import { Match, Player } from '@/db/schema';
 
 export default async function GamePage({
     params,
@@ -11,12 +11,23 @@ export default async function GamePage({
 }) {
 
     const game = await getGameByGameId(params.team_code) as Match;
-    
     //console.log('Match: ', game);
+    
+    const players: Player[] = game.gameData.players.map((player) => player);
+
+    const hostTeamId = players.find((player) => player.host === true)?.teamId;
+    //console.log(hostTeamId);
+
+    function filterEnemyTeam(player: Player) {
+        return player.teamId !== hostTeamId;
+    }
+
+    const enemyTeam = players.filter(filterEnemyTeam);
+    //console.log(enemyTeam);
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center p-24">
-            <GameCard game={game} />
+            <GameCard enemyTeam={enemyTeam} />
         </div>
     )
 }
--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -1,23 +1,11 @@
-import { Match, Player } from '@/db/schema';
+import { Player } from '@/db/schema';
 import { PlayerCard } from './PlayerCard';
 
 export const GameCard = ({
-    game,
+    enemyTeam,
 }: {
-    game: Match;
+    enemyTeam: Player[];
 }) => {
-
-    const players: Player[] = game.gameData.players.map((player) => player);
-
-    const hostTeamId = players.find((player) => player.host === true)?.teamId;
-    //console.log(hostTeamId);
-
-    function filterEnemyTeam(player: Player) {
-        return player.teamId !== hostTeamId;
-    }
-
-    const enemyTeam = players.filter(filterEnemyTeam);
-    //console.log(enemyTeam);
 
     return (
         <div className='flex flex-col justify-center items-center w-[320px]'>
