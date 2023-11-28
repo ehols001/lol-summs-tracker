03d73ff1e1465c4071f3a766a49999f7ebc94977
eholster <ehols001@odu.edu>
2023-11-28 11:43:49 -0500

    filters by enemy team now

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -2,18 +2,28 @@ import { Match, Player } from '@/db/schema';
 import { PlayerCard } from './PlayerCard';
 
 export const GameCard = ({
-        game,
+    game,
 }: {
-        game: Match;
+    game: Match;
 }) => {
-        
-        const players: Player[] = game.gameData.players.map((player) => player);
 
-        return (
-                <div className='flex flex-col justify-center items-center w-[320px]'>
-                        {players.map((player) => (
-                                <PlayerCard player={player} key={player.champion} />
-                        ))}
-                </div>
-        )
+    const players: Player[] = game.gameData.players.map((player) => player);
+
+    const hostTeamId = players.find((player) => player.host === true)?.teamId;
+    //console.log(hostTeamId);
+
+    function filterEnemyTeam(player: Player) {
+        return player.teamId !== hostTeamId;
+    }
+
+    const enemyTeam = players.filter(filterEnemyTeam);
+    //console.log(enemyTeam);
+
+    return (
+        <div className='flex flex-col justify-center items-center w-[320px]'>
+            {enemyTeam.map((player, index) => (
+                <PlayerCard player={player} key={index} />
+            ))}
+        </div>
+    )
 }
