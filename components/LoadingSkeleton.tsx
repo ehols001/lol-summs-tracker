262e8cbc16033db5e8130cf0d0fb2b8c06be3330
eholster <ehols001@odu.edu>
2023-11-28 16:21:14 -0500

    added little animation to loading

--- components/LoadingSkeleton.tsx
+++ components/LoadingSkeleton.tsx
@@ -1,9 +1,9 @@
 const Placeholders = () => {
     return (
         <div className='flex justify-evenly w-[90%] py-3'>
-            <div className='bg-gradient-to-br from-slate-400 to-slate-600 w-[70px] h-[70px] border border-gray-600 rounded-md'></div>
-            <div className='bg-gradient-to-br from-slate-400 to-slate-600 w-[70px] h-[70px] my-auto border border-gray-600 rounded-md'></div>
-            <div className='bg-gradient-to-br from-slate-400 to-slate-600 w-[70px] h-[70px] my-auto border border-gray-600 rounded-md'></div>
+            <div className='animate-pulse bg-gradient-to-br from-slate-400 to-slate-600 w-[70px] h-[70px] border border-gray-600 rounded-md'></div>
+            <div className='animate-pulse bg-gradient-to-br from-slate-400 to-slate-600 w-[70px] h-[70px] my-auto border border-gray-600 rounded-md'></div>
+            <div className='animate-pulse bg-gradient-to-br from-slate-400 to-slate-600 w-[70px] h-[70px] my-auto border border-gray-600 rounded-md'></div>
         </div>
     )
 }
