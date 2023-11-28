d6730247465c02effbb3205025318446824914b8
eholster <ehols001@odu.edu>
2023-11-28 11:41:03 -0500

    cleanup and create session conditional

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,33 @@
 'use client'
 
 import Link from 'next/link'
-import React from 'react';
+import { useState } from 'react';
 
 export default function Home() {
-    const [summonerNameValue, setSummonerNameValue] = React.useState('');
-    const [tagLineValue, setTagLineValue] = React.useState('');
+    const [summonerNameValue, setSummonerNameValue] = useState('');
+    const [tagLineValue, setTagLineValue] = useState('');
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center p-24">
             <input name='summonerName'
-                placeholder='Summoner'
-                className='bg-slate-400 m-2 p-2 rounded-lg placeholder-white'
+                placeholder='Summoner Name'
+                className='bg-slate-600 m-2 p-2 rounded-lg placeholder-white'
                 onChange={(e) => setSummonerNameValue(e.target.value)} />
 
             <input name='tagLine'
-                placeholder='Tag Line'
-                className='bg-slate-400 m-2 p-2 rounded-lg placeholder-white'
+                placeholder='Example: NA1'
+                className='bg-slate-600 m-2 p-2 rounded-lg placeholder-white'
                 onChange={(e) => setTagLineValue(e.target.value)} />
 
-            <Link className='bg-slate-400 m-2 p-2 rounded-lg' href={{ pathname: '/create-session', query: { summonerName: summonerNameValue, tagLine: tagLineValue } }}>
-                Create Session
-            </Link>
+            {(summonerNameValue === '' || tagLineValue === '') ?
+                <div className='bg-gray-300 text-gray-400 m-2 p-2 rounded-lg'>
+                    Create Session
+                </div>
+                :
+                <Link className='bg-slate-400 m-2 p-2 rounded-lg' href={{ pathname: '/create-session', query: { summonerName: summonerNameValue, tagLine: tagLineValue } }}>
+                    Create Session
+                </Link>
+            }
 
             <p className='pt-12'>Already have a game code?</p>
             <Link className='bg-slate-400 m-2 p-2 rounded-lg'
