15e227897754dda8c7e44b92912263497423cf55
eholster <ehols001@odu.edu>
2023-11-29 12:26:13 -0500

    normalized summoner name for comparison

--- utils/getMatchSession.ts
+++ utils/getMatchSession.ts
@@ -24,7 +24,7 @@ export default async function getMatchSession(
             champion: champion.name,
             championImageName: champion.image,
 
-            host: (summonerName === element.summonerName) ? true : false,
+            host: (summonerName.toLowerCase() === element.summonerName.toLowerCase()) ? true : false,
             summonerName: element.summonerName,
             teamId: element.teamId,
 
