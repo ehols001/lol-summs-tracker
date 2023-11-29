4f82193f7d5ad2c68f065f60f70686b32d77d531
eholster <ehols001@odu.edu>
2023-11-29 12:28:56 -0500

    added parallel routing for team tabs

---
+++ app/game/[team_code]/@team1/page.tsx
@@ -0,0 +1,23 @@
+import { getGameByGameId } from '@/app/api/game-data/getGame';
+import { GameCard } from '@/components/GameCard';
+import { Match, Player } from '@/db/schema';
+import Loading from '../loading';
+
+export default async function GamePage({
+    params,
+}: {
+    params: { 
+        team_code: string
+    };
+}) {
+
+    const game = await getGameByGameId(params.team_code) as Match;
+    const players: Player[] = game?.gameData.players.map((player) => player);
+    const team1: Player[] = players?.slice(0, 5);
+
+    return (
+        <div>
+            {game ? <GameCard players={team1} /> : <Loading />}
+        </div>
+    )
+}
--- app/game/[team_code]/layout.tsx
+++ app/game/[team_code]/layout.tsx
@@ -1,16 +1,37 @@
-import { Suspense } from "react"
+'use client'
+
+import { Suspense, useState } from "react"
 import Loading from "./loading"
 
 export default function GameLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
+    team1,
+    team2,
+}: {
+    team1: React.ReactNode;
+    team2: React.ReactNode;
+}) {
+
+    const [teamDisplay, setTeamDisplay] = useState(1);
+
     return (
-      <div>
-        <Suspense fallback={<Loading />}>
-            {children}
-        </Suspense>
-      </div>
+        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+            <div className='flex w-[265px]'>
+                <button
+                    className={teamDisplay === 1 ? 'w-1/2 p-2 text-slate-300 border-t border-gray-400 rounded-t-xl' : 'w-1/2 p-2 text-slate-500'}
+                    onClick={() => setTeamDisplay(1)}
+                >
+                    Team 1
+                </button>
+                <button
+                    className={teamDisplay === 2 ? 'w-1/2 p-2 text-slate-300 border-t border-gray-400 rounded-t-xl' : 'w-1/2 p-2 text-slate-500'}
+                    onClick={() => setTeamDisplay(2)}
+                >
+                    Team 2
+                </button>
+            </div>
+            <Suspense fallback={<Loading />}>
+                {teamDisplay === 1 ? team1 : team2}
+            </Suspense>
+        </div>
     )
-  }
+}
