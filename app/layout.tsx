f4f2771dc5846430692a3624261187b23f601f27
eholster <ehols001@odu.edu>
2023-11-29 12:24:58 -0500

    changed global font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Bitter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const bitter = Bitter({
+    subsets: ['latin'],
+    weight: '400'
+});
 
 export const metadata: Metadata = {
   title: 'Summoners Hub',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={bitter.className}>{children}</body>
     </html>
   )
 }
