f5723b07151f459fe3b7eb9e4c2c914398a82854
eholster <ehols001@odu.edu>
2023-11-30 11:15:15 -0500

    storing timeWhenUsed values correctly

--- app/api/game-data/updateGame.ts
+++ app/api/game-data/updateGame.ts
@@ -1,40 +1,37 @@
+'use server'
+
 import Game from '@/db/schema';
 import Connect from '@/lib/connect';
-import { cache } from 'react';
-
-export const revalidate = 3600;
 
 /**
  * Update a specific game given the gameId (will primarily be used to updated the timestamp of when summoner spells were used)
  * 
  * @param gameId the gameId of the game (not the id of the game)
  * @param playerIndex the index of the player whose summoner spell timer is being updated
- * @param summ the summoner spell that was used (either 1 slot or 2 slot)
- * 
- * @returns full updated game data including gameId, gameData, and the players array
+ * @param summNum the number summoner spell that was used (either 1 slot or 2 slot)
  */
-export const updateGame = cache(async (
+export async function updateGame(
         gameId: string,
         playerIndex: number,
-        summ: number,
-) => {
+        summNum: number,
+) {
         await Connect();
 
         try {
-                const updatedGame = await Game.findOne({ gameId: gameId });
+                const game = await Game.findOne({ gameId: gameId });
 
-                /* if(summ === 1) {
-                        game.gameData.players[playerIndex].timeWhenUsed1 = new Date;
-                } else if(summ === 2) {
-                        game.gameData.players[playerIndex].timeWhenUsed2 = new Date;
+                const player = game.gameData.players[playerIndex];
+                if(summNum === 1) {
+                        player.timeWhenUsed1 = new Date;
+                        await game.save();
+                        console.log(`Successfully updated ${player.champion}'s ${player.summ1} in game ${gameId} to time used: ${player.timeWhenUsed1.getTime()}`);
+                } else if(summNum === 2) {
+                        player.timeWhenUsed2 = new Date;
+                        await game.save();
+                        console.log(`Successfully updated ${player.champion}'s ${player.summ2} in game ${gameId} to time used: ${player.timeWhenUsed2.getTime()}`);
                 }
-                await game.save(); */
-                
-                console.log(`Successfully updated game with gameId: ${gameId}`);
-
-                return updatedGame;
 
         } catch(error) {
                 console.log(`Failed to update game with gameId: ${gameId}`, error);
         }
-});
+}
