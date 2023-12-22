6889ffb1512e8622a149c9b6fa2824979b12b834
eholster <ehols001@odu.edu>
2023-12-21 19:09:37 -0500

    updated deleteGame to firebase

--- app/api/game-data/deleteGame.ts
+++ app/api/game-data/deleteGame.ts
@@ -1,18 +1,16 @@
-import clientPromise from '@/lib/mongodb';
+import { db } from "@/lib/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
 
 /**
- * Delete a specific game given the gameId (will be used primarily when the game ends) 
+ * Delete a specific game given the gameId (used primarily when the game ends) 
  * 
  * @param gameId the gameId of the game (not the id of the game)
  */
 export async function deleteGame(gameId: string) {
     try {
-        const client = await clientPromise;
-        const db = client.db('GameDB');
+        const deletedGame = await deleteDoc(doc(db, 'games', gameId));
 
-        const game = await db.collection('games').deleteOne({ gameId: gameId });
-
-        console.log(`Successfully deleted game with gameId: ${gameId}`, game);
+        console.log(`Successfully deleted game with gameId: ${gameId}`, deletedGame);
 
     } catch (error) {
         console.log(`Failed to delete game with gameId: ${gameId}`, error);
